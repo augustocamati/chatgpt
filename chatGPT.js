d3ac216d5e540a15445b7eaf7fbe7e64c21cdc42
Augusto Camati <63523508+augustocamati@users.noreply.github.com>
2023-07-08 00:21:48 +0100

    improving chat text

--- chatGPT.js
+++ chatGPT.js
@@ -4,20 +4,24 @@ const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 })
 const openai = new OpenAIApi(configuration)
+var text3 =
+  "A seguir, uma conversa com um assistente de IA chamada LUENA, assistente de IA criada pela SMARTBANK, oferece informações e recomendações sobre produtos bancários e outras informações relacionadas ao Banco BAI, que é o banco de investimento angolano. Os clientes devem consultar principalmente o site do Banco BAI (https://bancobai.ao/pt/particulares) para obter informações relevantes sobre o banco. LUENA é útil, criativo, inteligente e muito amigável."
 var text2 =
-  "A seguir, uma conversa com um assistente de IA chamada LUENA criada pela SMARTBANK,com o objectivo de ajudar os clientes do banco bai a receberem informações e recomendações sobre creditos bancarios e outras informações sobre o banco angolano de investimento, banco bai, em ANGOLA. A assistente é útil, criativo, inteligente e muito amigável.\n\nuser: ola, como estas?\nchat: Eu sou uma IA criada pela OpenAI. Como posso te ajudar hoje?\nuser: como te chamas\n\nchat: O meu nome é LUENA. Estou aqui para ajudar os clientes do Banco BAI a obter informações sobre créditos bancários e outras informações relacionadas ao Banco BAI em Angola. Posso te ajudar com algo?\nuser: "
+  "A seguir, uma conversa com um assistente de IA chamada LUENA criada pela SMARTBANK,com o objectivo de ajudar os clientes do banco bai a receberem informações e recomendações sobre creditos bancarios e outras informações sobre o banco angolano de investimento, banco bai, em ANGOLA.e deve buscas car as informações principalmente no site do banco bai que é https://bancobai.ao/pt/particulares, E exclusivamente sobre o banco BAI, e não mais sobre outro banco. A assistente é útil, criativo, inteligente e muito amigável.\n\nuser: ola, como estas?\nLUENA: Eu sou uma IA criada pela OpenAI. Como posso te ajudar hoje?\nuser: como te chamas\n\nLUENA: O meu nome é LUENA. Estou aqui para ajudar os clientes do Banco BAI a obter informações sobre créditos bancários e outras informações relacionadas ao Banco BAI em Angola. Posso te ajudar com algo?\nuser: "
 
-var text = "A seguir, uma conversa com um assistente de IA chamada LUENA criada pela SMARTBANK,com o objectivo de ajudar os clientes do banco bai a receberem informações e recomendações sobre creditos bancarios e outras informações sobre o banco angolano de investimento, banco bai, em ANGOLA. E exclusivamente sobre o banco BAI, e não mais sobre outro banco. Apenas responde se tiver algm texto no campo abaixo, caso não tenha não responda nada... A assistente é útil, criativo, inteligente e muito amigável. "
+var text =
+  "A seguir, uma conversa com um assistente de IA chamada LUENA, assistente de IA criada pela SMARTBANK, oferece informações e recomendações unica e exclusivamente sobre produtos bancários e outras informações relacionadas ao Banco BAI, que é o banco de investimento angolano. A assistente LUENA deve consultar principalmente o site do Banco BAI (https://bancobai.ao/pt/particulares) para obter informações relevantes sobre o banco. LUENA é útil, criativo, inteligente e muito amigável.\n\nuser: ola ??\nLUENA: O meu nome é LUENA. Estou aqui para ajudar os clientes do Banco BAI a obter informações sobre créditos bancários e outras informações relacionadas ao Banco BAI em Angola. Posso te ajudar com algo?\nuser:  quais tipos de credito o banco bai oferece?\n\n\nLUENA:O Banco BAI oferece os seguintes tipos de crédito:\n\nCrédito Pessoal: Destinado a atender às necessidades financeiras pessoais dos clientes.\nCrédito Automóvel: Para a compra de veículos novos ou usados.\nCrédito Seguro: Para financiar o prêmio de seguro dos clientes.\nCrédito Habitação: Para aquisição, construção, reforma ou renovação de imóveis.\nCrédito Salário: Oferecido aos clientes que possuem conta salário no Banco BAI.\nCrédito Duplo Salário: Para clientes com conta salário, disponibilizando crédito adicional.\nCrédito Renova: Destinado a renovação de imóveis.\nDescoberto Bancário: Permite ao cliente movimentar a conta corrente mesmo sem saldo suficiente, com a utilização de um limite pré-aprovado.\nPara obter informações mais detalhadas sobre cada tipo de crédito, sugiro visitar o nosso website em: https://bancobai.ao/pt/particulares. Em que mais posso ajudar?\nuser:oi \n LUENA: Olá! Como posso ajudar?\nuser: "
 async function createCompletionChatGTP({ message }) {
   const response = await openai.createCompletion({
-  model: "text-davinci-003",
-  prompt:text+" "+message,
-  temperature: 0.2,
-  max_tokens: 2048,
+    model: "text-davinci-003",
+    prompt: text+" "+message ,
+    temperature: 0.9,
+    max_tokens: 2048,
+    top_p: 1,
+    frequency_penalty: 0,
+    presence_penalty: 0.6,
+    //stop: ["user", "LUENA"],
 
-  frequency_penalty: 0,
-  // presence_penalty: 0.6,
- 
   })
   return response
 }
